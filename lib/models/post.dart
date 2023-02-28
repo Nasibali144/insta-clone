668ca5611527f13ae87aee73bd58f09ef33f8cb2
Nasibali144 <abdiyevnasibali@gmail.com>
2023-02-28 18:11:04 +0500

    Initial commit

---
+++ lib/models/post.dart
@@ -0,0 +1,16 @@
+class Post {
+  int id;
+  String title;
+  String content;
+  DateTime createdAt;
+  int likeCount;
+  String mediaUrl;
+
+  // TODO: bitta oddiy constructor
+  // TODO: static flieds
+
+  // TODO: Object: elements
+  // TODO: compareTO
+  // TODO: your ide
+
+}
